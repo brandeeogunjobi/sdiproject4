fdf7bb3aeb56ce8d6914fcc503730953ce6a43df
brandeeogunjobi <ketaloginc@brandees-MacBook-Pro.local>
2014-08-23 21:22:20 -0700

    created my first phone number function reg exp.

--- rename-me.js
+++ rename-me.js
@@ -3,3 +3,27 @@
 //Brandee Ogunjobi
 //SDI Term 1408
 // Project 4
+
+
+
+//Does a string follow a 123-456-7890 pattern like a phone number?
+
+var numberPattern = function (pattern) {
+		var re = "/^\(?(\d{3})\)?[-]?(\d{3})[-]?(\d{4}$/",
+					test;
+					
+			test = regExp.test(pattern)
+		
+		if (test) {
+			return(pattern + " Patter is a phone number. ");
+		}
+		else {
+			return(pattern + " Pattern is not a valid number. ");
+			}
+			
+};
+		
+		
+		
+
+	
